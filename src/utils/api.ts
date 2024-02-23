e900c64d274191c850d15aecc731fe98b466e49c
Hamza MAEROF <hamzamaerof98@gmail.com>
2024-02-23 22:17:31 +0100

    convert images to webp

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,2 +1,2 @@
-// export const baseURL = "http://localhost:8000" ;
-export const baseURL = "https://my-json-server.typicode.com/HamzaMF7/car-rental-api" ;
+export const baseURL = "http://localhost:8000" ;
+// export const baseURL = "https://my-json-server.typicode.com/HamzaMF7/car-rental-api" ;
